2c1eef0e93efcf83a5c43ce2c6d0a2013685cfdb
qiuzhichang <qiu_zhi_chang@126.com>
2022-04-13 10:42:18 +0800

    add edit_rosbag.py

--- PythonClient/computer_vision/qzc_merge_ws/src/src/edit_rosbag.py
+++ PythonClient/computer_vision/qzc_merge_ws/src/src/edit_rosbag.py
@@ -2,6 +2,7 @@ import math
 import os
 import random
 import sys
+import shutil
 # sys.path.remove('/home/qiuzc/Documents/1_code/2_github/kimera/catkin_ws/devel/lib/python2.7/dist-packages')
 # import sh
 # sh.zsh('-c', 'source /home/qiuzc/Documents/1_code/2_github/kimera/python3_ros_ws/devel_isolated/setup.zsh')
@@ -80,10 +81,49 @@ map_label_to_color = np.vectorize(
     lambda x: id_color_dict.get(x, id_color_dict[255])
 )
 
-bag_name = '_2022-04-11-11-48-23_0.bag'  # 被修改的bag名
-out_bag_name = 'out_2022-04-11-11-48-23_0.bag'  # 修改后的bag名
+bag_name = '2022-04-12-16-22-58.bag'  # 被修改的bag名
+out_bag_name = 'out_2022-04-12-16-22-58_1.bag'  # 修改后的bag名
 dst_dir = '/Users/aqiu/Documents/AirSim/2022-03-07-02-02/'  # 使用路径
+WIDTH=960
+HEIGHT=540
+g_shape = (WIDTH, HEIGHT)
 
+
+topic_odom = '/local/odometry'
+topic_rgb = '/adu/camera_body_front_center/stereo/image_rect_color'
+topic_semantic_confidence = '/px/perception/semantic_confidence'
+topic_semantic_labels = '/px/perception/semantic_labels'
+topic_semantic_rgb = '/px/perception/visual/main'
+
+extract_bag_dir = dst_dir+bag_name[:-4]
+rgb_path = extract_bag_dir + '/rgb'
+semantic_confidence_path = extract_bag_dir + '/semantic_confidence'
+semantic_labels_path = extract_bag_dir + '/semantic_labels'
+semantic_labels_rgb_path = extract_bag_dir + '/semantic_labels_rgb'
+
+## Try to remove tree; if failed show an error using try...except on screen
+try:
+    shutil.rmtree(extract_bag_dir)
+except OSError as e:
+    print ("Error: %s - %s." % (e.filename, e.strerror))
+try:
+    os.makedirs(extract_bag_dir)
+    os.makedirs(rgb_path)
+    os.makedirs(semantic_confidence_path)
+    os.makedirs(semantic_labels_path)
+    os.makedirs(semantic_labels_rgb_path)
+except OSError:
+    if not os.path.isdir(extract_bag_dir):
+        raise
+
+odom_file = extract_bag_dir + '/odom.txt'
+odom_file_handle = open(odom_file, 'w')
+odom_file_handle.writelines("timestamp x y z qw qx qy qz"+"\n")
+
+# with open(bbs_file,'w') as f:    #设置文件对象
+#     f.writelines(header+"\n")
+#     for object_info in object_infos:
+#         f.writelines(object_info+"\n")                 #将字符串写入文件中
 
 with rosbag.Bag(dst_dir+out_bag_name, 'w') as outbag:
     stamp = None
@@ -103,29 +143,70 @@ with rosbag.Bag(dst_dir+out_bag_name, 'w') as outbag:
     status(40, 0)
     # ---------------------------- for debug end --------------------------------
     i = 0
+    cnt_dict={}
+    cnt_dict[1]=0
+    cnt_dict[2]=0
+    cnt_dict[3]=0
+    cnt_dict[4]=0
+
+
+
     for topic, msg, t in bag_in.read_messages():
-        outbag.write(topic, msg, msg.header.stamp)
-        if topic == '/px/perception/semantic_labels':
-            msg_bak = msg
-
-            cv_image = CvBridge().imgmsg_to_cv2(msg, "mono8")
-
-            topic = '/px/perception/visual/main'
-            semantic = np.asarray(map_label_to_color(cv_image))
-            semantic = np.stack(
-                (semantic[0], semantic[1], semantic[2]), axis=2)
-            semantic = semantic.astype(np.uint8)
-
-            # msg = CvBridge().cv2_to_imgmsg(semantic, "bgr8")
-            msg = CvBridge().cv2_to_imgmsg(semantic, "rgb8")
-
-            msg.header.stamp.secs = msg_bak.header.stamp.secs
-            msg.header.stamp.nsecs = msg_bak.header.stamp.nsecs
-            msg.header.frame_id = msg_bak.header.frame_id
-
-            outbag.write(topic, msg, msg.header.stamp)
-
         i=i+1
+        if topic == topic_odom:
+            pose ="{} {} {} {} {} {} {} {}".format(t.nsecs, msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z,
+                                                                    msg.pose.pose.orientation.w, msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
+                                                                    msg.pose.pose.orientation.z)
+            odom_file_handle.write(pose+"\n")
+        elif topic == topic_rgb:
+            cnt_dict[1]=cnt_dict[1]+1
+            cv_image = CvBridge().imgmsg_to_cv2(msg, "bgr8")
+            h=cv_image.shape[0]
+            w=cv_image.shape[1]
+            c=cv_image.shape[2]
+            cv_image=cv_image[0:h, 0:np.int32(w/2), 0:c]
+            cv_image = cv2.resize(cv_image, g_shape)
+            cv2.imwrite(os.path.normpath(os.path.join(rgb_path, str(cnt_dict[1]) + '.png')), cv_image)
+        elif topic == topic_semantic_confidence:
+            cnt_dict[2]=cnt_dict[2]+1
+            cv_image = CvBridge().imgmsg_to_cv2(msg, "mono8")
+            cv_image = cv2.resize(cv_image, g_shape)
+            cv2.imwrite(os.path.normpath(os.path.join(semantic_confidence_path, str(cnt_dict[2]) + '.png')), cv_image)
+        elif topic == topic_semantic_labels:
+            cnt_dict[3]=cnt_dict[3]+1
+            cv_image = CvBridge().imgmsg_to_cv2(msg, "mono8")
+            cv_image = cv2.resize(cv_image, g_shape)
+            cv2.imwrite(os.path.normpath(os.path.join(semantic_labels_path, str(cnt_dict[3]) + '.png')), cv_image)
+        elif topic == topic_semantic_rgb:
+            cnt_dict[4]=cnt_dict[4]+1
+            cv_image = CvBridge().imgmsg_to_cv2(msg, "bgr8")
+            cv_image = cv2.resize(cv_image, g_shape)
+            cv2.imwrite(os.path.normpath(os.path.join(semantic_labels_rgb_path, str(cnt_dict[4]) + '.png')), cv_image)
+
+
+        # write image
+        # outbag.write(topic, msg, msg.header.stamp)
+        if 0:
+            if topic == '/px/perception/semantic_labels':
+                msg_bak = msg
+
+                cv_image = CvBridge().imgmsg_to_cv2(msg, "mono8")
+
+                topic = '/px/perception/visual/main1'
+                semantic = np.asarray(map_label_to_color(cv_image))
+                semantic = np.stack(
+                    (semantic[0], semantic[1], semantic[2]), axis=2)
+                semantic = semantic.astype(np.uint8)
+
+                # msg = CvBridge().cv2_to_imgmsg(semantic, "bgr8")
+                msg = CvBridge().cv2_to_imgmsg(semantic, "rgb8")
+
+                msg.header.stamp.secs = msg_bak.header.stamp.secs
+                msg.header.stamp.nsecs = msg_bak.header.stamp.nsecs
+                msg.header.frame_id = msg_bak.header.frame_id
+
+                outbag.write(topic, msg, msg.header.stamp)
+
         percent = (i - start_index) / duration
         if percent - last_percent > interval:
             last_percent = percent
@@ -135,4 +216,7 @@ with rosbag.Bag(dst_dir+out_bag_name, 'w') as outbag:
             status(40, percent, i, remain_time)
 
 status(40, 1, duration, 0)
+
+odom_file_handle.close()
+
 print("finished")
